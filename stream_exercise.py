93246096a2a25703df55465023adc69bc703b08b
Chris Kenyon <cbkenyon89@gmail.com>
2019-09-25 02:38:43 -0700

    stream-exercise: test working

--- stream_exercise.py
+++ stream_exercise.py
@@ -48,20 +48,24 @@ class StreamProcessor(object):
     def process(self):
         """
         TODO: Implement the `process` method, as described above.
-        
+    
         :return: int
         """
-
+    
         count = 0  # How many two-digit numbers the `process` method has added
                    # together.
         total = 0  # The running total of sums.
+    
+        while count < 10 and total < 200:
+            digits = self._stream.read(2)
+            if len(digits) < 2:
+                break
+            if digits[0] == '0':
+                digits = digits[1]
 
-        # TODO: WRITE CODE HERE:
+            count += 1
 
-        # Just some example syntax, you can read two digits from the head of the
-        # stream using the following code:
-        #
-        # digits = self._stream.read(2)
-
+            n = int(digits)
+            total += n
 
         return count
